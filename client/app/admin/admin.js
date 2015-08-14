437f0cbe3e77766e64ebae3d0f7cd76a4381eb0b
moigithub <mcarlosman@gmail.com>
2015-08-14 10:32:13 -0500

    initial commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('basej3App')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
